11fabe86ad9aa421bcfcd0ce40f1fbd1d1daabc3
d4nif4bbr0 <fabretti30@gmail.com>
2024-09-22 20:07:12 +0200

    aggiunto bottone che aggiunge img

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let btn = document.querySelector('button');
 
+function loadImage(url,type,callback){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET',url);
+    xhr.responseType = type;
+
+    xhr.onload = function(){
+        callback(xhr.response);
+    };
+    xhr.send();
+}
+function displayImage(blob){
+    let objectURL = URL.createObjectURL(blob);
+
+    let image = document.createElement('img');
+    image.src = objectURL;
+    document.body.appendChild(image);
+}
+
+
+
+
 btn.addEventListener('click',()=>{
-    fetch('ananas.jpg');
-})
+    loadImage('ananas.jpg','blob',displayImage);
+});
